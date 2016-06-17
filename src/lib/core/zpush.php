f8dcc5f2b1207f37e5575ade53855d45aabdb899
Manfred Kutas <manfred@z-push.org>
2016-06-17 14:58:22 -0300

    ZP-962 Only unregister ZPush::IncludeBackend if it was previously registered.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/lib/core/zpush.php
+++ src/lib/core/zpush.php
@@ -570,9 +570,10 @@ class ZPush {
      * @return object     IBackend implementation
      */
     static public function GetBackend() {
-        $isIbar = false;
         // if the backend is not yet loaded, load backend drivers and instantiate it
         if (!isset(ZPush::$backend)) {
+            $isIbar = false;
+
             // Initialize our backend
             $ourBackend = @constant('BACKEND_PROVIDER');
 
@@ -598,10 +599,11 @@ class ZPush {
                 ZPush::$backend = new $ourBackend();
             else
                 throw new FatalMisconfigurationException(sprintf("Backend provider '%s' can not be loaded. Check configuration!", $ourBackend));
-        }
-        if ($isIbar) {
-            spl_autoload_unregister('\ZPush::IncludeBackend');
-            ZLog::Write(LOGLEVEL_DEBUG, "ZPush::GetBackend(): autoload unregister ZPush::IncludeBackend");
+
+            if ($isIbar) {
+                spl_autoload_unregister('\ZPush::IncludeBackend');
+                ZLog::Write(LOGLEVEL_DEBUG, "ZPush::GetBackend(): autoload unregister ZPush::IncludeBackend");
+            }
         }
         return ZPush::$backend;
     }
